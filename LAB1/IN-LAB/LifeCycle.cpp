74c5ba058c059205e3bf8f9dd32428de9c16ef89
EzaRasheed <42775084+EzaRasheed@users.noreply.github.com>
2020-01-18 16:10:27 -0500

    Add files via upload

---
+++ LAB1/IN-LAB/LifeCycle.cpp
@@ -0,0 +1,50 @@
+//Eza Rasheed
+//er6qt
+//LifeCycle.cpp
+//1-22-19
+
+#include <iostream>
+#include <string>
+#include "LifeCycle.h"
+using namespace std;
+
+
+// ------------------------------------------------  default constructor
+MyObject::MyObject(const char *n) : name(n) {
+    id = ++numObjs;
+    cout << "MyObject Default constructor: " << *this << endl;
+}
+
+// ---------------------------------------------------  copy constructor
+MyObject::MyObject(const MyObject& rhs) : name(rhs.name) {
+    id = ++numObjs;
+    cout << "MyObject Copy constructor:    " << *this << endl;
+}
+
+// --------------------------------------------------------  destructor
+MyObject::~MyObject() {
+    cout << "MyObject Destructor:          " << *this << endl;
+}
+
+string MyObject::getName() const {
+        return name; }
+
+void MyObject::setName(const string newName){
+	name = newName;
+}
+
+// ----------------------------------------------------------  print out
+ostream& operator<<(ostream& output, const MyObject& obj) {
+    // output in format:  ("object name",id)
+    return output << "(\"" << obj.name << "\"," << obj.id << ")";
+}
+
+//---------------------------------------------------- static variables
+int MyObject::numObjs = 0;  // static member for all objects in class
+static MyObject staticObj("I'm static, outside of main");
+
+//---------------------------------------------------------- prototypes
+MyObject getMaxMyObj(const MyObject o1, const MyObject o2);
+int cmpMyObj(const MyObject o1, const MyObject o2);
+void swapMyObj(MyObject& o1, MyObject& o2);
+
